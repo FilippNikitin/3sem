417a33a1b06b12ef43cb39304cae34077f5f0e5d
FilippNikitin <filipp.nikitin.96@gmail.com>
2015-10-15 13:34:07 +0300

    deadline 20.10.15 fixed

--- Рабочий стол/Прога/Server/server.c
+++ Рабочий стол/Прога/Server/server.c
@@ -8,6 +8,7 @@
 #include <sys/sem.h>
 #include <unistd.h>
 #define N 1
+#define FileName "klient.c"
 
 /*
     Здесь много повторяющегося кода, если надо, разобью на функции
@@ -30,49 +31,22 @@ struct mymsgbufout
     long int result;
 };
 
-void* my_thread(void* arg);
+int semid;
+int msqid;
+
+void* ProcessInquiry(void* arg);
+key_t GenerateKey();
+int CreateSemaphore();
+void ChangeSemaphore(int semid, int n);
+int CreateQueue();
 
 int main()
 {
-    char pathname[] = "klient.c";
-    key_t key;
-
-    int msqid;
     struct mymsgbufin mybufin;
-    struct mymsgbufout mybufout;
-
-    int semid;
-    struct sembuf mybuf;
-
     pthread_t thread_id;
-
-    if ((key = ftok(pathname, 0)) < 0)
-    {
-        printf("Can\'t generate key\n");
-        exit(-1);
-    }
-
-    if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0)
-    {
-        printf("Can`t get semid\n");
-        exit(-1);
-    }
-
-    mybuf.sem_op = N;
-    mybuf.sem_flg = 0;
-    mybuf.sem_num = 0;
-
-    if (semop(semid , &mybuf , 1) < 0)
-    {
-        printf("Can`t wait for condition\n");
-        exit(-1);
-    }
-
-    if((msqid = msgget(key, 0666 | IPC_CREAT)) < 0)
-    {
-        printf("Can\'t get msqid\n");
-        exit(-1);
-    }
+    semid = CreateSemaphore();
+    msqid = CreateQueue();
+    ChangeSemaphore(semid, N);
 
     while (1)
     {
@@ -81,61 +55,17 @@ int main()
             printf("Can\'t receive message from queue\n");
             exit(-1);
         }
-        pthread_create(&thread_id, (pthread_attr_t *)NULL , my_thread, &mybufin);
+        pthread_create(&thread_id, (pthread_attr_t *)NULL , ProcessInquiry, &mybufin);
     }
     return 0;
 }
 
-void* my_thread(void* arg)
+void* ProcessInquiry(void* arg)
 {
-    /*
-     * client 
-     * Если вы осознанно написали klient, то ваше дело.
-     */
-    char pathname[] = "klient.c";
-    key_t key;
-
-    int msqid;
-    struct mymsgbufin mybufin;
+    struct mymsgbufin mybufin = *((struct mymsgbufin *)arg);
     struct mymsgbufout mybufout;
-    mybufin = *((struct mymsgbufin *)arg);
 
-    int semid;
-    struct sembuf mybuf;
-
-    if ((key = ftok(pathname, 0)) < 0)
-    {
-        printf("Can\'t generate key\n");
-        exit(-1);
-    }
-
-    if ((semid = semget(key, 1, 0666)) < 0)
-    {
-        printf("Can`t get semid\n");
-        exit(-1);
-    }
-
-    /* Сделайте глобальную переменную, либо передавайте semid в эту ф-ю в качестве параметра: так даже лучше будет. Не надо копист практиковать. */
-    
-    mybuf.sem_op = -1;
-    mybuf.sem_flg = 0;
-    mybuf.sem_num = 0;
-
-    if (semop(semid , &mybuf , 1) < 0)
-    {
-        printf("Can`t wait for condition\n");
-        exit(-1);
-    }
-
-    /*
-     * Та же проблема, что и с semid.
-     */
-    if((msqid = msgget(key, 0666 | IPC_CREAT)) < 0)
-    {
-        printf("Can\'t get msqid\n");
-        exit(-1);
-    }
-
+    ChangeSemaphore(semid, -1);
     mybufout.result = mybufin.data.a * mybufin.data.b;
     mybufout.mtype = mybufin.mtype;
 
@@ -145,12 +75,56 @@ void* my_thread(void* arg)
         msgctl(msqid, IPC_RMID, (struct msqid_ds*)NULL);
         exit(-1);
     }
+    sleep(30);
+    ChangeSemaphore(semid, 1);
+}
 
-    mybuf.sem_op = 1;
+key_t GenerateKey()
+{
+    key_t key;
+    char pathname[] = FileName;
+    if ((key = ftok(pathname, 0)) < 0)
+    {
+        printf("Can\'t generate key\n");
+        exit(-1);
+    }
+    return key;
+}
+
+int CreateSemaphore()
+{
+    key_t key = GenerateKey();
+    int semid;
+    struct sembuf mybuf;
+    if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0)
+    {
+        printf("Can`t get semid\n");
+        exit(-1);
+    }
+    return semid;
+}
+
+int CreateQueue()
+{
+    key_t key = GenerateKey();
+    int msqid;
+    struct mymsgbufin mybufin;
+    struct mymsgbufout mybufout;
+    if((msqid = msgget(key, 0666 | IPC_CREAT)) < 0)
+    {
+        printf("Can\'t get msqid\n");
+        exit(-1);
+    }
+    return msqid;
+}
+
+void ChangeSemaphore(int semid, int n)
+{
+    struct sembuf mybuf;
+    mybuf.sem_op = n;
     mybuf.sem_flg = 0;
     mybuf.sem_num = 0;
-
-    if (semop(semid , &mybuf , 1) < 0)
+    if (semop(semid, &mybuf, 1) < 0)
     {
         printf("Can`t wait for condition\n");
         exit(-1);
